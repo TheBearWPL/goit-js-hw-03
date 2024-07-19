ccaaa475b44db4e03fefa92ae76b39f81b2137a0
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-19 17:07:24 +0200

    Initial commit

---
+++ task-3.js
@@ -0,0 +1,11 @@
+function filterArray(numbers, value) {
+  const filteredNumbers = [];
+
+  for (const number of numbers) {
+    if (number > value) {
+      filteredNumbers.push(number);
+    }
+  }
+
+  return filteredNumbers;
+}
