f673dea9af366505ec7db843e09fd802eec92f20
TheBearWPL <maksymilian_jarosz@op.pl>
2024-07-19 19:15:48 +0200

    Update task-1.js

--- task-1.js
+++ task-1.js
@@ -1,11 +1,4 @@
 "use strict";
 function slugify(title) {
-  return String(title)
-    .normalize("NFKD") // split accented characters into their base characters and diacritical marks
-    .replace(/[\u0300-\u036f]/g, "") // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-    .trim() // trim leading or trailing whitespace
-    .toLowerCase() // convert to lowercase
-    .replace(/[^a-z0-9 -]/g, "") // remove non-alphanumeric characters
-    .replace(/\s+/g, "-") // replace spaces with hyphens
-    .replace(/-+/g, "-"); // remove consecutive hyphens
+  return String(title).toLowerCase();
 }
